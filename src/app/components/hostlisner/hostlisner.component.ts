ce7f6d78c4fbd5bafb919af8ac990438d9bfffa7
Charu Jaiswal <Charu.Jaiswal@srmtechsol.com>
2019-09-16 11:18:58 +0530

    16-sep-19

---
+++ src/app/components/hostlisner/hostlisner.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Directive, HostListener, Renderer, ElementRef } from '@angular/core';
+
+@Directive({selector: '[appdemo]'})
+
+
+
+@Component({
+  selector: 'app-hostlisner',
+  // template: '<button counting>Increment</button>',
+  templateUrl: './hostlisner.component.html',
+  styleUrls: ['./hostlisner.component.css']
+})
+export class HostlisnerComponent implements OnInit {
+  changecolor: any;
+  ishovering: boolean;
+
+  constructor( private el: ElementRef,private renderer: Renderer) {
+  
+   }
+ @HostListener('click') dosome (){
+ alert('lost listner click')
+   //this.changecolor('red');
+ }
+ @HostListener('mouseover') onHover() {
+  window.alert("hover");
+}
+
+  ngOnInit() {
+  }
+
+}
